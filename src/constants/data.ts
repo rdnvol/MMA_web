87eec7dfb2af81ec57d30597582073336d7ea08f
rdnvol <rdnv.ol20@gmail.com>
2023-06-18 20:59:23 +0300

    Update: getAvailableTimeSlots

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -42,6 +42,7 @@ export type Event = {
   lessonType?: LESSON_TYPES;
   label?: string;
   isFloating?: boolean;
+  isHalfTime?: boolean;
   orderedCoaches?: COACHES[];
 };
 
@@ -232,18 +233,18 @@ const lessonsData: Lesson[] = [
   //   startTime: "12:00",
   //   endTime: "13:30",
   // },
-  // {
-  //   id: "mon-5",
-  //   lessonType: lessonTypesData[6],
-  //   participants: [participantsData[4]],
-  //   date: format(currentWeek[1], DATE_FORMAT),
-  //   startTime: "9:00",
-  //   endTime: "10:00",
-  //   // orderedCoaches: [coachesData[0], coachesData[1]],
-  // },
+  {
+    id: "mon-5",
+    lessonType: lessonTypesData[2],
+    participants: [participantsData[5]],
+    date: format(currentWeek[1], DATE_FORMAT),
+    startTime: "8:30",
+    endTime: "9:30",
+    // orderedCoaches: [coachesData[0], coachesData[1]],
+  },
   {
     id: "mon-6",
-    lessonType: lessonTypesData[6],
+    lessonType: lessonTypesData[2],
     participants: [participantsData[4]],
     date: format(currentWeek[1], DATE_FORMAT),
     startTime: "9:00",
@@ -441,6 +442,7 @@ export function dumpLessonToEvent(lesson: Lesson): Event {
     lessonType: lesson.lessonType.type,
     isFloating:
       lesson.lessonType.coaches.length > 1 && !lesson.orderedCoaches?.length,
+    isHalfTime: lesson.lessonType.coachBusyLevel === BUSY_LEVELS.HALF,
     orderedCoaches: lesson.orderedCoaches?.map(
       (coach) => coach.name as COACHES
     ),
--- src/lib/TimeSlot.ts
+++ src/lib/TimeSlot.ts
@@ -1,18 +1,19 @@
 import { COACHES } from "../constants/data";
-import { LESSON_TYPES } from "../models";
+import TimeSlotsMap from "./TimeSlotsMap";
 
 export default class TimeSlot {
   constructor(
     public startTime: number,
     public duration: number,
+    private parent: TimeSlotsMap,
     public isFloating?: boolean,
     private coachToBusiness: Record<string, number> = {}
   ) {}
 
   addCoachBusiness(
     coach: COACHES,
-    lessonType: LESSON_TYPES,
-    isFloating: boolean
+    isFloating: boolean,
+    isHalfTime: boolean
   ): void {
     if (this.isFloating === undefined) {
       this.isFloating = isFloating;
@@ -20,8 +21,7 @@ export default class TimeSlot {
       throw new Error("INCONSISTENT_DATA");
     }
 
-    let business: number =
-      lessonType === LESSON_TYPES.SPLIT || isFloating ? 0.5 : 1;
+    let business = isHalfTime || isFloating ? 0.5 : 1;
 
     if (!this.coachToBusiness[coach]) {
       this.coachToBusiness[coach] = 0;
@@ -32,5 +32,94 @@ export default class TimeSlot {
 
   getCoachBusiness(coach: COACHES) {
     return this.coachToBusiness[coach];
+  }
+
+  getFloatingAvailability(coaches: COACHES[]): boolean {
+    const prev = this.parent.getPrevTimeSlot(this.startTime);
+    const next = this.parent.getNextTimeSlot(this.startTime);
+
+    if (coaches.some((coach: COACHES) => this.getCoachBusiness(coach) >= 1.5)) {
+      return false;
+    }
+
+    if (
+      coaches.every((coach: COACHES) => {
+        if (prev && !prev.isFloating && prev.getCoachBusiness(coach) > 1) {
+          return true;
+        }
+
+        if (next && !next.isFloating && next.getCoachBusiness(coach) > 1) {
+          return true;
+        }
+
+        return false;
+      })
+    ) {
+      return false;
+    }
+
+    return true;
+  }
+
+  getHalfTimeAvailability(coaches: COACHES[]): boolean {
+    const prev = this.parent.getPrevTimeSlot(this.startTime);
+    const next = this.parent.getNextTimeSlot(this.startTime);
+
+    if (
+      this.isFloating &&
+      coaches.some((coach: COACHES) => this.getCoachBusiness(coach) >= 1.5)
+    ) {
+      return false;
+    }
+
+    if (
+      !this.isFloating &&
+      coaches.some((coach: COACHES) => this.getCoachBusiness(coach) >= 1.5)
+    ) {
+      return false;
+    }
+
+    if (
+      coaches.some((coach: COACHES) => {
+        if (prev && prev.getCoachBusiness(coach) >= 2) {
+          return true;
+        }
+        if (next && next.getCoachBusiness(coach) >= 2) {
+          return true;
+        }
+
+        return false;
+      })
+    ) {
+      return false;
+    }
+
+    return true;
+  }
+
+  getFullTimeAvailability(coaches: COACHES[]): boolean {
+    const prev = this.parent.getPrevTimeSlot(this.startTime);
+    const next = this.parent.getNextTimeSlot(this.startTime);
+
+    if (coaches.some((coach: COACHES) => this.getCoachBusiness(coach) >= 1)) {
+      return false;
+    }
+
+    if (
+      coaches.some((coach: COACHES) => {
+        if (prev && !prev.isFloating && prev.getCoachBusiness(coach) >= 1.5) {
+          return true;
+        }
+        if (next && !next.isFloating && next.getCoachBusiness(coach) >= 1.5) {
+          return true;
+        }
+
+        return false;
+      })
+    ) {
+      return false;
+    }
+
+    return true;
   }
 }
